b3d7418a238149d535b3b0fd6391c2962a2b5ce7
Daniel Nolan <DanNolan99@gmail.com>
2019-01-13 14:25:09 -0500

    reddir

--- deploy.js
+++ deploy.js
@@ -1,7 +1,11 @@
 const AdmZip = require('adm-zip');
+const fs = require('fs-extra');
 
-console.log('running?')
+console.log('running?');
 
 const zip = new AdmZip();
 zip.addLocalFile('pizza.js');
 zip.writeZip('upload.zip');
+
+const contents = fs.readdirSync('./');
+console.log({ contents });
